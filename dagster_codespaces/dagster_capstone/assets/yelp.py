ed07837a2bf06c363385e7173d8d1a4dface74d4
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-04-02 19:40:43 -0400

    update yelp.py

--- dagster_codespaces/dagster_capstone/assets/yelp.py
+++ dagster_codespaces/dagster_capstone/assets/yelp.py
@@ -4,25 +4,26 @@ import os
 
 @asset(
     io_manager_key="polars_parquet_io_manager",
-    required_resource_keys={"s3"},
-    ins={'kaggle_file': AssetIn()}
+    required_resource_keys={"s3"}
 )
-def json_to_parquet(context, kaggle_file: str) -> pl.DataFrame:
+def json_to_parquet(context) -> pl.DataFrame:
+    s3_bucket = "de-capstone-project"
+    s3_key = "yelp/raw/"
+    json_filename = "yelp_academic_dataset_business.json"
+    s3_path = f"s3://{s3_bucket}/{s3_key}/{json_filename}"
 
-    json_filename = "specific_file.json"
-    json_file_path = os.path.join(kaggle_file, json_filename)
-    context.log.info(f"json path {json_file_path}")
+    context.log.info(f"json path {s3_path}")
 
     # Read the JSON file into a Polars DataFrame
-    df = pl.read_json(json_file_path)
+    df = pl.read_json(s3_path)
 
     # Define the S3 path for the output Parquet file
-    s3_bucket = "de-capstone-project"
-    s3_key = f"pipeline-output/{os.path.basename(json_filename).replace('.json', '.parquet')}"
-    context.log.info(f"Writing Parquet to: s3://{s3_bucket}/{s3_key}")
+    s3_key = f"yelp/processed/"
+    parquet_filename = json_filename.replace('.json', '.parquet')
+    context.log.info(f"Writing Parquet to: s3://{s3_bucket}/{s3_key}/{parquet_filename}")
 
     # Write the DataFrame to S3 in Parquet format
-    s3_path = f"s3://{s3_bucket}/{s3_key}"
+    s3_path = f"s3://{s3_bucket}/{s3_key}/{parquet_filename}"
     df.write_parquet(s3_path)
 
     return df
