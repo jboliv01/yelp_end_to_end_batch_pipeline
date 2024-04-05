dd8fab10a86f49f272103ccb4e6b36080ce2a240
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-04-04 20:34:19 -0400

    reverting back to fs

--- dagster_codespaces/dagster_capstone/assets/yelp.py
+++ dagster_codespaces/dagster_capstone/assets/yelp.py
@@ -66,12 +66,12 @@ def yelp_data(context):
         context.log.info(f'reading JSON body')
 
         context.log.info(f'loading dataframe lazily')
-        lazy_df = pl.scan_ndjson(s3_path, storage_options={'s3': s3})
+        # lazy_df = pl.scan_ndjson(s3_path, storage_options={'s3': s3})
 
-        # with fs.open(s3_path, mode='rb') as f:
-        #     context.log.info(f's3 object type: {type(f)}')
-        #     context.log.info(f'loading dataframe lazily')
-        #     lazy_df = pl.scan_ndjson(f)
+        with fs.open(s3_path, mode='rb') as f:
+            context.log.info(f's3 object type: {type(f)}')
+            context.log.info(f'loading dataframe lazily')
+            lazy_df = pl.scan_ndjson(f)
 
         context.log.info(f'yielding output: {asset_name}')
         yield Output(lazy_df, asset_name)
