68d13cce5d71c58ebb8501058b3295e307402360
Jonah Oliver <jboliv01@gmail.com>
2024-04-18 23:11:11 -0700

    print DBT_DIRECTORY

--- dagster_capstone/resources/__init__.py
+++ dagster_capstone/resources/__init__.py
@@ -7,6 +7,8 @@ from dagster_dbt import DbtCliResource
 
 from ..assets.constants import DBT_DIRECTORY
 
+print("DBT_DIRECTORY before usage in DbtCliResource:", DBT_DIRECTORY)
+
 dbt_resource = DbtCliResource(
     project_dir=DBT_DIRECTORY,
 )
