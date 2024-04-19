0b24cd39103f36d0bd32bba0a9f62c5e53170453
Jonah Oliver <jboliv01@gmail.com>
2024-04-18 23:25:08 -0700

    remove os.getenv

--- dagster_capstone/resources/__init__.py
+++ dagster_capstone/resources/__init__.py
@@ -15,10 +15,6 @@ dbt_resource = DbtCliResource(
     project_dir=DBT_DIRECTORY,
 )
 
-database_resource = DuckDBResource(
-    database=EnvVar("DUCKDB_DATABASE"),
-)
-
 if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
     database_resource = DuckDBResource(
         database=EnvVar("MOTHERDUCK_DATABASE"),
