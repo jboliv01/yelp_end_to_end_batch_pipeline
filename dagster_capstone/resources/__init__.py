7cb4f137f292294db1be120bb2f9eb37157ec4f6
Jonah Oliver <jboliv01@gmail.com>
2024-04-19 07:04:14 -0700

    revert resources and try again

--- dagster_capstone/resources/__init__.py
+++ dagster_capstone/resources/__init__.py
@@ -8,7 +8,7 @@ from dagster_dbt import DbtCliResource
 from ..assets.constants import DBT_DIRECTORY
 
 database_resource = DuckDBResource(
-    database=EnvVar("DUCKDB_DATABASE").get_value()
+    database=EnvVar("DUCKDB_DATABASE")
 )
 
 dbt_resource = DbtCliResource(
@@ -17,12 +17,12 @@ dbt_resource = DbtCliResource(
 
 if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
     database_resource = DuckDBResource(
-        database=EnvVar("MOTHERDUCK_DATABASE").get_value(),
-        connection_config={"motherduck_token": EnvVar("MOTHERDUCK_TOKEN").get_value()},
+        database=EnvVar("MOTHERDUCK_DATABASE"),
+        connection_config={"motherduck_token": EnvVar("MOTHERDUCK_TOKEN")},
     )
     session = boto3.Session(
-        aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID").get_value(),
-        aws_secret_access_key=EnvVar("AWS_SECRET_ACCESS_KEY").get_value(),
+        aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=EnvVar("AWS_SECRET_ACCESS_KEY"),
         region_name=EnvVar("AWS_REGION").get_value(),
     )
     smart_open_config = {"client": session.client("s3")}
@@ -31,3 +31,39 @@ else:
     database_resource = DuckDBResource(
         database=EnvVar("DUCKDB_DATABASE"),
     )
+
+
+
+# import os
+# import boto3
+
+# from dagster import EnvVar
+# from dagster_duckdb import DuckDBResource
+# from dagster_dbt import DbtCliResource
+
+# from ..assets.constants import DBT_DIRECTORY
+
+# database_resource = DuckDBResource(
+#     database=EnvVar("DUCKDB_DATABASE").get_value()
+# )
+
+# dbt_resource = DbtCliResource(
+#     project_dir=DBT_DIRECTORY,
+# )
+
+# if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
+#     database_resource = DuckDBResource(
+#         database=EnvVar("MOTHERDUCK_DATABASE").get_value(),
+#         connection_config={"motherduck_token": EnvVar("MOTHERDUCK_TOKEN").get_value()},
+#     )
+#     session = boto3.Session(
+#         aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID").get_value(),
+#         aws_secret_access_key=EnvVar("AWS_SECRET_ACCESS_KEY").get_value(),
+#         region_name=EnvVar("AWS_REGION").get_value(),
+#     )
+#     smart_open_config = {"client": session.client("s3")}
+# else:
+#     smart_open_config = {}
+#     database_resource = DuckDBResource(
+#         database=EnvVar("DUCKDB_DATABASE"),
+#     )
