99f3f32577728d9c7ace45dd67afac407188584e
Jonah Oliver <jboliv01@gmail.com>
2024-04-18 23:24:53 -0700

    update to EnvVar

--- dagster_capstone/resources/__init__.py
+++ dagster_capstone/resources/__init__.py
@@ -11,27 +11,27 @@ database_resource = DuckDBResource(
     database=EnvVar("DUCKDB_DATABASE")
 )
 
-# dbt_resource = DbtCliResource(
-#     project_dir=DBT_DIRECTORY,
-# )
+dbt_resource = DbtCliResource(
+    project_dir=DBT_DIRECTORY,
+)
 
-# database_resource = DuckDBResource(
-#     database=os.getenv("DUCKDB_DATABASE", "data/staging/data.duckdb"),
-# )
+database_resource = DuckDBResource(
+    database=EnvVar("DUCKDB_DATABASE"),
+)
 
-# if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
-#     database_resource = DuckDBResource(
-#         database=os.getenv("MOTHERDUCK_DATABASE"),
-#         connection_config={"motherduck_token": os.getenv("MOTHERDUCK_TOKEN")},
-#     )
-#     session = boto3.Session(
-#         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-#         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-#         region_name=os.getenv("AWS_REGION"),
-#     )
-#     smart_open_config = {"client": session.client("s3")}
-# else:
-#     smart_open_config = {}
-#     database_resource = DuckDBResource(
-#         database=os.getenv("DUCKDB_DATABASE"),
-#     )
+if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
+    database_resource = DuckDBResource(
+        database=EnvVar("MOTHERDUCK_DATABASE"),
+        connection_config={"motherduck_token": EnvVar("MOTHERDUCK_TOKEN")},
+    )
+    session = boto3.Session(
+        aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=EnvVar("AWS_SECRET_ACCESS_KEY"),
+        region_name=EnvVar("AWS_REGION"),
+    )
+    smart_open_config = {"client": session.client("s3")}
+else:
+    smart_open_config = {}
+    database_resource = DuckDBResource(
+        database=EnvVar("DUCKDB_DATABASE"),
+    )
