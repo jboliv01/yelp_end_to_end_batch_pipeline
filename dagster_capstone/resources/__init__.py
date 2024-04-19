49d121c191a1cc1cc159612e6c81e44114441479
Jonah Oliver <jboliv01@gmail.com>
2024-04-18 23:20:46 -0700

    commenting out database-resource

--- dagster_capstone/resources/__init__.py
+++ dagster_capstone/resources/__init__.py
@@ -7,29 +7,31 @@ from dagster_dbt import DbtCliResource
 
 from ..assets.constants import DBT_DIRECTORY
 
-print("DBT_DIRECTORY before usage in DbtCliResource:", DBT_DIRECTORY)
-
-dbt_resource = DbtCliResource(
-    project_dir=DBT_DIRECTORY,
-)
-
 database_resource = DuckDBResource(
-    database=os.getenv("DUCKDB_DATABASE", "data/staging/data.duckdb"),
+    database=EnvVar("DUCKDB_DATABASE")
 )
 
-if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
-    database_resource = DuckDBResource(
-        database=os.getenv("MOTHERDUCK_DATABASE"),
-        connection_config={"motherduck_token": os.getenv("MOTHERDUCK_TOKEN")},
-    )
-    session = boto3.Session(
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("AWS_REGION"),
-    )
-    smart_open_config = {"client": session.client("s3")}
-else:
-    smart_open_config = {}
-    database_resource = DuckDBResource(
-        database=os.getenv("DUCKDB_DATABASE"),
-    )
+# dbt_resource = DbtCliResource(
+#     project_dir=DBT_DIRECTORY,
+# )
+
+# database_resource = DuckDBResource(
+#     database=os.getenv("DUCKDB_DATABASE", "data/staging/data.duckdb"),
+# )
+
+# if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
+#     database_resource = DuckDBResource(
+#         database=os.getenv("MOTHERDUCK_DATABASE"),
+#         connection_config={"motherduck_token": os.getenv("MOTHERDUCK_TOKEN")},
+#     )
+#     session = boto3.Session(
+#         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+#         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+#         region_name=os.getenv("AWS_REGION"),
+#     )
+#     smart_open_config = {"client": session.client("s3")}
+# else:
+#     smart_open_config = {}
+#     database_resource = DuckDBResource(
+#         database=os.getenv("DUCKDB_DATABASE"),
+#     )
